67d13edefdae18e71521f26e5d98699688a6882a
harish.kumar <harish.kumar@impressico.com>
2023-01-17 17:00:14 +0530

    Implement add asset request API

--- components/ProductDetail/ProductDetail.tsx
+++ components/ProductDetail/ProductDetail.tsx
@@ -1,8 +1,11 @@
-import { View, Image } from "react-native";
+import { View, Image, ToastAndroid } from "react-native";
 import { Text, TextInput, Button } from "@react-native-material/core";
-import React from "react";
+import { useState } from "react";
+
+import * as SecureStore from "expo-secure-store";
 
 import ProductDetailStyles from "./ProductDetail.styles";
+import { createAPIEndpoint, ENDPOINTS } from "../../services/api.service";
 
 interface ProductDetailProps {
   navigation?: any;
@@ -12,6 +15,28 @@ interface ProductDetailProps {
 const ProductDetail: React.FC<ProductDetailProps> = ({ route, navigation }) => {
   {
     const { product } = route.params;
+    const [text, setText] = useState("");
+
+    const handleSubmit = async () => {
+      const token = await SecureStore.getItemAsync("access");
+      if (token) {
+        const response = await createAPIEndpoint(
+          ENDPOINTS.ASSETREQUESTADD
+        ).create(
+          {
+            asset_id: product.serial_number,
+            remarks: text,
+          },
+          token
+        );
+        if (response) {
+          navigation.goBack();
+          navigation.goBack();
+          navigation.navigate("Product List");
+          ToastAndroid.show("Request submit successfully.", ToastAndroid.SHORT);
+        }
+      }
+    };
     return product ? (
       <View style={ProductDetailStyles.container}>
         <Text style={ProductDetailStyles.headline}>{product.asset_name}</Text>
@@ -19,13 +44,16 @@ const ProductDetail: React.FC<ProductDetailProps> = ({ route, navigation }) => {
           source={{ uri: `${product.image_url}` }}
           style={ProductDetailStyles.image}
         />
-        <TextInput style={ProductDetailStyles.textbox} variant="outlined" />
+        <TextInput
+          style={ProductDetailStyles.textbox}
+          variant="outlined"
+          defaultValue={text}
+          onChangeText={(newText) => setText(newText)}
+        />
         <Button
           style={ProductDetailStyles.button}
           title="Submit"
-          onPress={() => {
-            navigation.push("Product List");
-          }}
+          onPress={handleSubmit}
         />
       </View>
     ) : (
